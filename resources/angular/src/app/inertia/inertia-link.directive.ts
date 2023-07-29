633d126564119b756df6702927d8cfbfb7602511
Martin Hettiger <martin@hettiger.com>
2023-07-29 17:30:21 +0200

    Refactor inertia link directive

--- resources/angular/src/app/inertia/inertia-link.directive.ts
+++ resources/angular/src/app/inertia/inertia-link.directive.ts
@@ -1,41 +1,18 @@
-import { Directive, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
-import { fromEvent, mergeMap, Observable, Subject, takeUntil } from 'rxjs';
+import { Directive, HostListener, Input } from '@angular/core';
 import { InertiaNavigationService } from './inertia-navigation.service';
 
 @Directive({
   selector: '[inertiaLink]'
 })
-export class InertiaLinkDirective implements OnInit, OnDestroy {
+export class InertiaLinkDirective {
 
   @Input('inertiaLink') href = '';
 
-  click$: Observable<MouseEvent>;
-  unsubscribe$ = new Subject<void>();
-
   constructor(
-    private elementRef: ElementRef,
-    private inertiaNavigationService: InertiaNavigationService,
-  ) {
-    this.click$ = fromEvent<MouseEvent>(this.elementRef.nativeElement, 'click', { capture: true });
+    private inertiaNavigationService: InertiaNavigationService
+  ) {}
+
+  @HostListener('click') onClick() {
+    this.inertiaNavigationService.navigate(this.href).subscribe();
   }
-
-  ngOnInit(): void {
-    this.click$
-      .pipe(
-        takeUntil(this.unsubscribe$),
-        mergeMap((event) => {
-          event.stopImmediatePropagation();
-          event.preventDefault();
-
-          return this.inertiaNavigationService.navigate(this.href);
-        })
-      )
-      .subscribe();
-  }
-
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-  }
-
 }
