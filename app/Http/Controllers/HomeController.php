5959888fb5b50ad8ba6ee9f26259b3bd31769acf
Martin Hettiger <martin@hettiger.com>
2023-07-29 12:34:34 +0200

    Implement home route

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    public function __invoke()
+    {
+        return Inertia::render('Home');
+    }
+}
